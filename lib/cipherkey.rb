1da7a0223029ec333c33920d67888c4b2bc4484d
Nathan Keller <keller.nathan@gmail.com>
2020-01-09 12:58:43 -0700

    Create seperate to pairs method. Passes both tests

--- lib/cipherkey.rb
+++ lib/cipherkey.rb
@@ -1,4 +1,19 @@
 class CipherKey
 
+  def seperate_to_pairs(key = nil)
+    if key == nil
+      @code_key = rand(10000..99999)
+    else
+      @code_key = key
+    end
+    numbers = @code_key.to_s.split(//)
+    numbers.reduce({}) do |pair, number|
+    pair[:a] = numbers[0..1].join.to_i
+    pair[:b] = numbers[1..2].join.to_i
+    pair[:c] = numbers[2..3].join.to_i
+    pair[:d] = numbers[3..4].join.to_i
 
+    pair
+    end
+  end
 end
