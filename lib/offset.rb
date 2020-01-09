74132a2ae9abdb01032bbbe7d7965f6a61b84770
Nathan Keller <keller.nathan@gmail.com>
2020-01-09 12:44:17 -0700

    Crete offset_seperated method

--- lib/offset.rb
+++ lib/offset.rb
@@ -4,5 +4,21 @@ class Offset
     Time.now.strftime("%d%m%y")
   end
 
+  def offset_seperated(date = nil)
+    last_four =
+    if date == nil
+      (generate_date.to_i * generate_date.to_i) % 10000
+    else
+      (date.to_i * date.to_i) % 10000
+    end
 
+    last_four_seperate = last_four.to_s.split(//)
+
+    split = {
+      :a => last_four_seperate[0].to_i,
+      :b => last_four_seperate[1].to_i,
+      :c => last_four_seperate[2].to_i,
+      :d => last_four_seperate[3].to_i
+      }
+    end
 end
