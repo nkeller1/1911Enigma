724a5c0f9a24ee883bc84a7229bd3a7c868816a6
Nathan Keller <keller.nathan@gmail.com>
2020-01-09 12:30:41 -0700

    Create enigma class and test it exists passes

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,2 +1,14 @@
 require_relative 'testhelper'
 require_relative '../lib/enigma'
+
+class EnigmaTest < Minitest::Test
+
+  def setup
+    @enigma = Enigma.new
+  end
+
+  def test_it_exist
+    assert_instance_of Enigma, @enigma
+  end
+
+end
