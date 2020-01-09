d181ffcc83b4491b0697d9b40b0fd99f39f0136e
Nathan Keller <keller.nathan@gmail.com>
2020-01-09 12:54:34 -0700

    Create test for seperating the key into paired hash. Test with a 0 in fron and without.

--- test/cipherkey_test.rb
+++ test/cipherkey_test.rb
@@ -9,4 +9,12 @@ class CipherKeyTest < Minitest::Test
   def test_it_exists
     assert_instance_of CipherKey, @cipherkey
   end
+
+  def test_it_sepeates_to_pairs_with_a_given_key
+    pairing = {:a =>12,:b => 23, :c => 34, :d => 45}
+    assert_equal pairing, @cipherkey.seperate_to_pairs("12345")
+
+    zero_pair = {:a =>2,:b => 20, :c => 3, :d => 30}
+    assert_equal zero_pair, @cipherkey.seperate_to_pairs("02030")
+  end
 end
