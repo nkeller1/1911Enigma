468aac8241fe19a88cc57355aa622eb38197ef51
Nathan Keller <keller.nathan@gmail.com>
2020-01-09 21:54:25 -0700

    Create encryptmessage class and test. Add to enigma.encrypt

--- lib/encryptmessage.rb
+++ lib/encryptmessage.rb
@@ -12,9 +12,9 @@ class EncryptMessage
 
   def convert(letter)
     convert = []
-    letter_array = []
-    letter_array << letter
-    letter_array.each do |char|
+    letter_into_array = []
+    letter_into_array << letter
+    letter_into_array.each do |char|
       if alpha_with_index.include?(char)
         convert.push alpha_with_index[char]
       else
@@ -22,5 +22,20 @@ class EncryptMessage
       end
     end
     convert
+  end
+
+  def encrypt_single_letter(letter, shift)
+    convert(letter).map do |number|
+      if number.is_a?(String)
+        number = number
+      elsif number + shift >= 27
+        combine = number += shift
+        combine = combine % 27
+        combine = alpha_with_index.key(combine)
+      else
+        number += shift
+        number = alpha_with_index.key(number)
+      end
+    end.first
   end
 end
--- test/encryptmessage_test.rb
+++ test/encryptmessage_test.rb
@@ -16,16 +16,26 @@ class EncryptTest < Minitest::Test
   end
 
   def test_alpha_with_index
-    #option for module here with decrypt
+    #option for module later
     assert_equal 27, @encryptmessage.alphabet.length
   end
 
-  def test_convert
+  def test_convert_letter_into_number
+    #option for module later
     assert_equal [26], @encryptmessage.convert(" ")
     assert_equal [7], @encryptmessage.convert("h")
     assert_equal [4], @encryptmessage.convert("e")
     assert_equal [11], @encryptmessage.convert("l")
     assert_equal [11], @encryptmessage.convert("l")
     assert_equal ["!"], @encryptmessage.convert("!")
+  end
+
+  def test_encrypt_single_letter
+    assert_equal " ", @encryptmessage.encrypt_single_letter(" ", 108)
+    assert_equal "k", @encryptmessage.encrypt_single_letter("h", 3)
+    assert_equal "e", @encryptmessage.encrypt_single_letter("e", 27)
+    assert_equal "e", @encryptmessage.encrypt_single_letter("l", 20)
+    assert_equal "d", @encryptmessage.encrypt_single_letter("l", 73)
+    assert_equal "!", @encryptmessage.encrypt_single_letter("!", 29)
   end
 end
