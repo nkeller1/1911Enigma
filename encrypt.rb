e55d912a8b1dbc23345a9328f0a13258d50c9d89
Nathan Keller <keller.nathan@gmail.com>
2020-01-10 12:51:58 -0700

    encrypt.rb now encrypts a .txt file

--- encrypt.rb
+++ encrypt.rb
@@ -1,3 +1,11 @@
+require_relative './lib/enigma'
+require_relative './lib/cipherkey'
+require_relative './lib/offset'
+require_relative './lib/encryptmessage'
+require_relative './lib/decryptmessage'
+
+enigma = Enigma.new
+
 handle = File.open(ARGV[0], "r")
 
 incoming_text = handle.read
@@ -6,7 +14,7 @@ handle.close
 
 puts incoming_text
 
-encrypted_text = incoming_text.upcase
+encrypted_text = enigma.encrypt(incoming_text)
 
 writer = File.open(ARGV[1], "w")
 
